a8c7abdf6755080e1a6188f8a5d258ba28d0af50
PaBoba <140555335+PaBoba@users.noreply.github.com>
2023-08-23 16:25:12 -0700

    Changed starter code to generate a password with a minimum of 5 characters

--- Assets/script.js
+++ Assets/script.js
@@ -2,12 +2,19 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = "";
+  const passwordText = document.querySelector("#password");
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+"
+  var passwordLength = Math.random() * 10 + 4;
+    //  Math.floor(Math.random() * passwordLength) + 4;
+  for (var i = 0; i <= passwordLength; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+    }
 
   passwordText.value = password;
 
