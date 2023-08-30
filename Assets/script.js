d5113146c1b98e9c52279d7840822fcdcead5c32
PaBoba <140555335+PaBoba@users.noreply.github.com>
2023-08-30 16:24:35 -0700

    Changed the code to pull from an array to generate password

--- Assets/script.js
+++ Assets/script.js
@@ -8,13 +8,25 @@ const generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = "";
   const passwordText = document.querySelector("#password");
-  const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+"
-  var passwordLength = Math.random() * 10 + 4;
+  const finalPassword = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!',
+    '@', '#', '$', '%', '^', '&', '*', '(', ')', '+'
+  ]
+  // console.log(finalPassword.length);
+  // Generate a random number between 4 and 128
+  var passwordLength = Math.floor(Math.random() * 124 + 4);
+  console.log(passwordLength);
     //  Math.floor(Math.random() * passwordLength) + 4;
-  for (var i = 0; i <= passwordLength; i++) {
-    const randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber + 1);
+  for (var i = 0; i < passwordLength; i++) {
+    let randomNumber = (Math.floor(Math.random() * (finalPassword.length-1)));
+    // console.log(randomNumber);
+    let arrayString = finalPassword[randomNumber];
+    password = password.concat(arrayString);
     }
+
+  
 
   passwordText.value = password;
 
